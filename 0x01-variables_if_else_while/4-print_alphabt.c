c440ef2bda4cfbdb2d06c07c1dd1a97545e88491
Lesh <mulwalenah@gmail.com>
2022-06-03 10:04:02 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char mycase;
+
+	for (mycase = 'a'; mycase <= 'z'; mycase++)
+	{
+		if (mycase != 'e' && mycase != 'q')
+		{
+			putchar(mycase);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
