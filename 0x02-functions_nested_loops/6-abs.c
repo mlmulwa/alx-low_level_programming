a50513d6b9930c941deb7bf4ec5fc08b84788877
Lesh <mulwalenah@gmail.com>
2022-06-08 13:12:47 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - print absolute value
+ * @n: number being tested
+ * Return: Always 0 (Success)
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+	return (0);
+}
