a9ba9144495c95074689e0c8c90f154ff6a417c0
Lesh <mulwalenah@gmail.com>
2022-06-08 11:05:21 -0700

    Task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - check the code for letters whether lower case or upper case
+ * @c: letter being tested
+ * Return: Always 0 (Success) 
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+		return (1);
+	else
+		return (0);
+}
