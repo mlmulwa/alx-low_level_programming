198ecb3a58ba4adccc2e0bd24ede76f254a3ae26
Lesh <mulwalenah@gmail.com>
2022-06-09 14:53:47 -0700

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -10,9 +10,9 @@ void positive_or_negative(int i)
 {
 	if (i > 0)
 		printf("%d is positive\n", i);
-	if (i < 0)
-		printf("%d is negative\n", i);
 	if (i == 0)
 		printf("%d is zero\n", i);
+	if (i < 0)
+		printf("%d is negative\n", i);
 	return (0);
 }
