1f60d96ac359d80d39663aa01b372eb0653b79ff
Lesh <mulwalenah@gmail.com>
2022-06-09 15:23:47 -0700

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 
 /**
@@ -14,12 +13,12 @@ void positive_or_negative(int i)
 	{
 		printf("%d is positive\n", i);
 	}
-	else if (i == 0)
+	else if (i < 0)
 	{
-		printf("%d is zero\n", i);
+		printf("%d is negative\n", i);
 	}
 	else
 	{
-		printf("%d is negative\n", i);
+		printf("%d is zero\n", i);
 	}
 }
