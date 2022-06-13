b894d2797555e09aace1e60ac14e8beec6c61a44
Lesh <mulwalenah@gmail.com>
2022-06-13 15:41:50 -0700

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one character out of two of a string
+ * @str: char being printed
+ */
+void puts2(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		if ((a % 2) == 0)
+		{
+			_putchar(str[a]);
+		}
+		a++;
+	}
+	_putchar('\n');
+}
