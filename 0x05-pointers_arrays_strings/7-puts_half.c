ea806c8173dd90b99229d5a6b0c25ce0769bdb59
Lesh <mulwalenah@gmail.com>
2022-06-13 15:57:04 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,16 +9,18 @@ void puts_half(char *str)
 	int ct, even, neg;
 
 	for (ct = 0; str[ct] != '\0'; ct++)
-		;
-	if ((ct % 2) == 0)
 	{
-		for (even = ct / 2; str[even] != '\0'; even++)
-			_putchar(str[even]);
-	}
-	else
-	{
-		for (neg = (ct - 1) / 2; str[neg] != '\0'; neg++)
-			_putchar(str[neg + 1]);
+
+		if ((ct % 2) == 0)
+		{
+			for (even = ct / 2; str[even] != '\0'; even++)
+				_putchar(str[even]);
+		}
+		else
+		{
+			for (neg = (ct - 1) / 2; str[neg] != '\0'; neg++)
+				_putchar(str[neg + 1]);
+		}
 	}
 	_putchar('\n');
 }
