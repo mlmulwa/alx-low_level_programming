dc10e6613ff534638e02b11f42eba4bcf5fe5821
Lesh <mulwalenah@gmail.com>
2022-06-22 15:53:41 -0700

    Task 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,15 +14,15 @@ int wildcmp(char *s1, char *s2)
 		return (true);
 	if (*s1 == '*')
 	{
-		while (*(first + 1) == '*')
+		while (*(s1 + 1) == '*')
 			s1++;
 	}
 	if (*s1 == '*' && *(s1 + 1) != '\0' && *s2 == '\0')
 		return (false);
 	if (*s1 == '?' || *s1 == *s2)
-		return (match(s1 + 1, s2 + 1));
+		return (wildcmp(s1 + 1, s2 + 1));
 	if (*s1 == '*')
-		return (match(s1 + 1, s2) || match(s1, s2 + 1));
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	return (false);
 }
 
